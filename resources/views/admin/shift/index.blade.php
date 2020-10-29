700978418d8d95addf6cda520d433bada68d662b
abbas88 <abbas2a2@gmail.com>
2020-10-29 18:25:11 +0500

    shifts

--- resources/views/admin/shift/index.blade.php
+++ resources/views/admin/shift/index.blade.php
@@ -5,7 +5,7 @@
 @section('breadcrumb')
     <ul class="breadcrumb breadcrumb-transparent breadcrumb-dot font-weight-bold p-0 my-2 font-size-sm">
         <li class="breadcrumb-item">
-            <a href="{{ route('admin.shift.index')}}" class="text-muted">Employees</a>
+            <a href="{{ route('admin.shift.index')}}" class="text-muted">Shift</a>
         </li>
     </ul>
 @endsection
@@ -21,48 +21,14 @@
                     <div class="card card-custom">
                         <div class="card-header flex-wrap border-0 pt-6 pb-0">
                             <div class="card-title">
-                                <h3 class="card-label">Employees
-                                    <span class="d-block text-muted pt-2 font-size-sm">All employees</span></h3>
+                                <h3 class="card-label">Shift
+                                    <span class="d-block text-muted pt-2 font-size-sm">All Shifts</span></h3>
                             </div>
                             <div class="card-toolbar">
                                 <!--begin::Dropdown-->
                                 <div class="dropdown dropdown-inline mr-2">
-                                    <button type="button" class="btn btn-light-primary font-weight-bolder dropdown-toggle"
-                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <span class="svg-icon svg-icon-md">
-                                            <!--begin::Svg Icon | path:assets/media/svg/icons/Design/PenAndRuller.svg-->
-                                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                                width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                                    <rect x="0" y="0" width="24" height="24" />
-                                                    <path
-                                                        d="M3,16 L5,16 C5.55228475,16 6,15.5522847 6,15 C6,14.4477153 5.55228475,14 5,14 L3,14 L3,12 L5,12 C5.55228475,12 6,11.5522847 6,11 C6,10.4477153 5.55228475,10 5,10 L3,10 L3,8 L5,8 C5.55228475,8 6,7.55228475 6,7 C6,6.44771525 5.55228475,6 5,6 L3,6 L3,4 C3,3.44771525 3.44771525,3 4,3 L10,3 C10.5522847,3 11,3.44771525 11,4 L11,19 C11,19.5522847 10.5522847,20 10,20 L4,20 C3.44771525,20 3,19.5522847 3,19 L3,16 Z"
-                                                        fill="#000000" opacity="0.3" />
-                                                    <path
-                                                        d="M16,3 L19,3 C20.1045695,3 21,3.8954305 21,5 L21,15.2485298 C21,15.7329761 20.8241635,16.200956 20.5051534,16.565539 L17.8762883,19.5699562 C17.6944473,19.7777745 17.378566,19.7988332 17.1707477,19.6169922 C17.1540423,19.602375 17.1383289,19.5866616 17.1237117,19.5699562 L14.4948466,16.565539 C14.1758365,16.200956 14,15.7329761 14,15.2485298 L14,5 C14,3.8954305 14.8954305,3 16,3 Z"
-                                                        fill="#000000" />
-                                                </g>
-                                            </svg>
-                                            <!--end::Svg Icon-->
-                                        </span>Export</button>
-                                    <button type="button" class="btn btn-light-primary font-weight-bolder"
-                                        aria-haspopup="true" aria-expanded="false">
-                                        <span class="svg-icon svg-icon-md">
-                                            <!--begin::Svg Icon | path:assets/media/svg/icons/Design/PenAndRuller.svg-->
-                                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                                width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                                    <rect x="0" y="0" width="24" height="24" />
-                                                    <path
-                                                        d="M3,16 L5,16 C5.55228475,16 6,15.5522847 6,15 C6,14.4477153 5.55228475,14 5,14 L3,14 L3,12 L5,12 C5.55228475,12 6,11.5522847 6,11 C6,10.4477153 5.55228475,10 5,10 L3,10 L3,8 L5,8 C5.55228475,8 6,7.55228475 6,7 C6,6.44771525 5.55228475,6 5,6 L3,6 L3,4 C3,3.44771525 3.44771525,3 4,3 L10,3 C10.5522847,3 11,3.44771525 11,4 L11,19 C11,19.5522847 10.5522847,20 10,20 L4,20 C3.44771525,20 3,19.5522847 3,19 L3,16 Z"
-                                                        fill="#000000" opacity="0.3" />
-                                                    <path
-                                                        d="M16,3 L19,3 C20.1045695,3 21,3.8954305 21,5 L21,15.2485298 C21,15.7329761 20.8241635,16.200956 20.5051534,16.565539 L17.8762883,19.5699562 C17.6944473,19.7777745 17.378566,19.7988332 17.1707477,19.6169922 C17.1540423,19.602375 17.1383289,19.5866616 17.1237117,19.5699562 L14.4948466,16.565539 C14.1758365,16.200956 14,15.7329761 14,15.2485298 L14,5 C14,3.8954305 14.8954305,3 16,3 Z"
-                                                        fill="#000000" />
-                                                </g>
-                                            </svg>
-                                            <!--end::Svg Icon-->
-                                        </span>Import</button>
+                                    
+                                    
                                     <!--begin::Dropdown Menu-->
                                     <div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">
                                         <!--begin::Navigation-->
@@ -117,10 +83,31 @@
                                 </div>
                                 <!--end::Dropdown-->
                                 <!--begin::Button-->
-                                <a href="{{ route('admin.shift.create') }}" class="btn btn-primary font-weight-bolder text-center">
+                                <a href="" data-toggle="modal" data-target="#exampleModal" class="btn btn-primary font-weight-bolder text-center">
                                     <i class="fas fa-plus"></i>
                                     Add New
                                 </a>
+                                <form action="post" method="POST" action="{{ route('admin.employee.store') }}">
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                          <div class="modal-content">
+                                            <div class="modal-header">
+                                              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                              </button>
+                                            </div>
+                                            <div class="modal-body">
+                                              ...
+                                            </div>
+                                            <div class="modal-footer">
+                                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                              <button type="button" class="btn btn-primary">Save changes</button>
+                                            </div>
+                                          </div>
+                                        </div>
+                                    </div>
+                                </form>
                                 <!--end::Button-->
                             </div>
                         </div>
@@ -140,20 +127,7 @@
                                                     </span>
                                                 </div>
                                             </div>
-                                            <div class="col-md-4 my-2 my-md-0">
-                                                <div class="d-flex align-items-center">
-                                                    <label class="mr-3 mb-0 d-none d-md-block">Designation</label>
-                                                    <select class="form-control" id="kt_datatable_search_status">
-                                                        <option value="">All</option>
-                                                        <option value="1">Team Leader</option>
-                                                        <option value="2">Garphic Designer</option>
-                                                        <option value="3">IOS Developer</option>
-                                                        <option value="4">Android Developer</option>
-                                                        <option value="5">PHP Developer</option>
-                                                        <option value="6">Front-End Developer</option>
-                                                    </select>
-                                                </div>
-                                            </div>
+                                            
                                             
                                         </div>
                                     </div>
@@ -168,12 +142,10 @@
                             <table class="datatable datatable-bordered datatable-head-custom" id="kt_datatable">
                                 <thead>
                                     <tr>
-                                        <th title="Field #1">Full Name</th>
-                                        <th title="Field #2">Phone</th>
-                                        <th title="Field #3">CNIC</th>
-                                        <th title="Field #4">Designation</th>
-                                        <th title="Field #5">Department</th>
-                                        <th title="Field #6">Email</th>
+                                        <th title="Field #1">Name</th>
+                                        <th title="Field #2">Working Hours</th>
+                                        <th title="Field #3">Start Time</th>
+                                        <th title="Field #4">End Time</th>
                                         <th title="Field #6">Actions</th>
                                         <th title="Field #7"></th>
                                     </tr>
@@ -184,10 +156,14 @@
                                         {{-- <td>{{ $item->firstName .' '. $item->lastName }}
                                         </td>
                                         <td>{{ $item->emergencyPhone }}</td>
-                                        <td>{{ $item->cnic }}</td>
+                                        <td>{{ $item->cnic }}</td>  
                                         <td>{{ $item->designation->name }}</td>
                                         <td>{{ $item->department->deptName }}</td>
                                         <td>{{ $item->email }}</td> --}}
+                                        <td>Ali</td>
+                                        <td>8</td>
+                                        <td>10</td>
+                                        <td>7</td>
                                         <td>
                                             <div class="btn-group" role="group" aria-label="Basic example">
 
