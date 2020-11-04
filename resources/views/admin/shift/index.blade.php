c56aeef92f4b86c2d03125f38378faeaf898aa26
Rashid-RR <razarashid171@gmail.com>
2020-11-04 18:53:29 +0500

    Shift detail btn change

--- resources/views/admin/shift/index.blade.php
+++ resources/views/admin/shift/index.blade.php
@@ -363,8 +363,6 @@
                                                         <div class="modal-footer">
                                                             <button type="button" class="btn btn-secondary"
                                                                 data-dismiss="modal">Close</button>
-                                                            <button type="submit" class="btn btn-primary">Save
-                                                                changes</button>
                                                         </div>
                                                     </div>
                                                 </div>
